9d25fc2a9872e9cebca0694823df4d70ce146d90
modeverv <modeverv@gmail.com>
2020-05-20 00:24:46 +0900

    me

--- init.el
+++ init.el
@@ -32,3 +32,115 @@
   (spacemacs/setup-startup-hook)
   (require 'server)
   (unless (server-running-p) (server-start)))
+
+
+;;:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; spacemacsに乗っからないというか設定方法がわからない
+;;:;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; font
+(set-face-attribute 'default nil :height 180)
+;; \M-+ で拡大
+(global-set-key [(meta ?+)] (lambda () (interactive) (text-scale-increase 1)))
+;; \M-- で縮小
+(global-set-key [(meta ?-)] (lambda () (interactive) (text-scale-decrease 1)))
+;; \M-0 でデフォルトに戻す
+(global-set-key [(meta ?0)] (lambda () (interactive) (text-scale-increase 0)))
+;; dired
+;;;; ナイスなdired
+(require 'dired-x  nil t)
+(add-hook 'dired-load-hook (lambda () (load "dired-x")))
+;;;; 再帰コピー再帰削除
+(setq dired-recursive-copies 'always)
+(setq dired-recursive-deletes 'always)
+;;;; diredからファイル名編集
+(require 'wdired nil t)
+(define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
+;;;; ちょっと見
+(require 'bf-mode nil t)
+(setq bf-mode-browsing-size 20000)
+;; meta/super
+(setq ns-command-modifier (quote meta))
+(setq ns-alternate-modifier (quote super))
+;; frame
+(setq default-frame-alist
+      (append '((width                . 85)  ; フレーム幅
+                (height               . 38 ) ; フレーム高
+             ;; (left                 . 70 ) ; 配置左位置
+             ;; (top                  . 28 ) ; 配置上位置
+                (line-spacing         . 0  ) ; 文字間隔
+                (left-fringe          . 10 ) ; 左フリンジ幅
+                (right-fringe         . 11 ) ; 右フリンジ幅
+                (menu-bar-lines       . 1  ) ; メニューバー
+                (tool-bar-lines       . nil  ) ; ツールバー
+                (vertical-scroll-bars . nil  ) ; スクロールバー
+                (scroll-bar-width     . 17 ) ; スクロールバー幅
+                (cursor-type          . box) ; カーソル種別
+                (alpha                . 90) ; 透明度
+                ) default-frame-alist) )
+(setq initial-frame-alist default-frame-alist)
+
+;; フレーム タイトル
+(setq frame-title-format
+      '("emacs " emacs-version (buffer-file-name " - %f")))
+;; 文字コード関連
+;; デフォルトの文字コード
+(set-default-coding-systems 'utf-8-unix)
+;; テキストファイル／新規バッファの文字コード
+(prefer-coding-system 'utf-8-unix)
+;; ファイル名の文字コード
+(set-file-name-coding-system 'utf-8-unix)
+;; キーボード入力の文字コード
+(set-keyboard-coding-system 'utf-8-unix)
+;; サブプロセスのデフォルト文字コード
+(setq default-process-coding-system '(undecided-dos . utf-8-unix))
+;; 環境依存文字 文字化け対応
+(set-charset-priority 'ascii 'japanese-jisx0208 'latin-jisx0201
+                      'katakana-jisx0201 'iso-8859-1 'cp1252 'unicode)
+(set-coding-system-priority 'utf-8 'euc-jp 'iso-2022-jp 'cp932)
+;; cp932エンコードの表記変更
+(coding-system-put 'cp932 :mnemonic ?P)
+(coding-system-put 'cp932-dos :mnemonic ?P)
+(coding-system-put 'cp932-unix :mnemonic ?P)
+(coding-system-put 'cp932-mac :mnemonic ?P)
+;; UTF-8エンコードの表記変更
+(coding-system-put 'utf-8 :mnemonic ?U)
+(coding-system-put 'utf-8-with-signature :mnemonic ?u)
+;; 改行コードの表記追加
+(setq eol-mnemonic-dos       ":Dos ")
+(setq eol-mnemonic-mac       ":Mac ")
+(setq eol-mnemonic-unix      ":Unx ")
+(setq eol-mnemonic-undecided ":??? ")
+;;;; misc
+;; 同一バッファ名にディレクトリ付与
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-buffer-name-style 'post-forward-angle-brackets)
+(setq uniquify-ignore-buffers-re "*[^*]+*")
+;; auto-save
+(require 'auto-save-buffers-enhanced)
+(setq auto-save-buffers-enhanced-interval 1)
+(setq auto-save-buffers-enhanced-quiet-save-p t)
+(auto-save-buffers-enhanced t)
+;; scratch persist
+(persistent-scratch-setup-default)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; key
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; helm-mini
+(global-set-key (kbd "C-x j") 'helm-mini)
+(global-set-key (kbd "C-x C-j") 'helm-mini)
+;; helm-show-kill-ring
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "M-s a") 'helm-ag)
+;; kill buffer
+(global-set-key (kbd "C-x C-k") 'kill-buffer)
+;; 色
+;;dotspacemacs-themes '(spolsky
+;;                      spacemacs-dark
+;;                      spacemacs-light)
+;;dotspacemacs-additional-packages '(
+;;                                   ;; auto-save
+;;                                   auto-save-buffers-enhanced
+;;                                   ;; scratch
+;;                                   persistent-scratch
+;;                                   )
